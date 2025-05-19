dbc39f5301801d18a77a773e158e40cfaee36565
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-19 09:42:00 +0200

    GET all + GET by id endpoints on Restaurant

---
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Entities;
+
+namespace RestaurantAPI.Controllers;
+
+[Route("api/restaurant")]
+public class RestaurantController(RestaurantDbContext dbContext) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<Restaurant>> GetAll()
+    {
+        var restaurants = dbContext.Restaurants.ToList();
+        return Ok(restaurants);
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<IEnumerable<Restaurant>> Get([FromRoute] int id)
+    {
+        var restaurant = dbContext.Restaurants
+            .FirstOrDefault(r => r.Id == id);
+
+        return restaurant is not null
+            ? Ok(restaurant)
+            : NotFound();
+    }
+}
