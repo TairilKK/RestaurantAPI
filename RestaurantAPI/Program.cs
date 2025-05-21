9e08107d39f885cca2176681508d47b2af33d320
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-21 15:01:24 +0200

    Cors

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -85,6 +85,15 @@ builder.Services.AddScoped<RequestTimeMiddleware>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontEndClient", policyBuilder =>
+    {
+        policyBuilder.AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins(builder.Configuration["AllowedOrigins"]);
+    });
+});
 
 builder.Host.UseNLog();
 
@@ -95,6 +104,7 @@ using (var scope = app.Services.CreateScope())
     seeder.Seed();
 }
 
+app.UseCors("FrontEndClient");
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseMiddleware<RequestTimeMiddleware>();
 app.UseAuthentication();
